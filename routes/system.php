6e1eede14a05d338a061c5e5b49e454d2edbcf1f
Scorias <mostafa.samy203@gmail.com>
2022-02-13 00:38:52 +0200

    fix routes

--- routes/system.php
+++ routes/system.php
@@ -16,41 +16,41 @@ $router->group(['prefix' => 'sys-bin'], function () use ($router) {
     $router->group(['prefix' => 'admin', 'middleware' => 'auth.admin'], function () use ($router) {
         $router->group(['prefix' => 'subscriptions'], function () use ($router) {
             $router->group(['middleware' => ['filter.json']], function () use ($router) {
-                $router->post('/', 'Auth\SubscriptionController@CreateSubscription');
-                $router->put('/{subscription_id}', 'Auth\SubscriptionController@UpdateSubscription');
+                $router->post('/', 'VolistxTeam\VSkeletonKernel\Http\Controllers\SubscriptionController@CreateSubscription');
+                $router->put('/{subscription_id}', 'VolistxTeam\VSkeletonKernel\Http\Controllers\SubscriptionController@UpdateSubscription');
 
 
-                $router->post('/{subscription_id}/personal-tokens', 'Auth\PersonalTokenController@CreatePersonalToken');
-                $router->put('/{subscription_id}/personal-tokens/{token_id}', 'Auth\PersonalTokenController@UpdatePersonalToken');
+                $router->post('/{subscription_id}/personal-tokens', 'VolistxTeam\VSkeletonKernel\Http\Controllers\PersonalTokenController@CreatePersonalToken');
+                $router->put('/{subscription_id}/personal-tokens/{token_id}', 'VolistxTeam\VSkeletonKernel\Http\Controllers\PersonalTokenController@UpdatePersonalToken');
 
             });
 
-            $router->delete('/{subscription_id}', 'Auth\SubscriptionController@DeleteSubscription');
-            $router->get('/', 'Auth\SubscriptionController@GetSubscriptions');
-            $router->get('/{subscription_id}', 'Auth\SubscriptionController@GetSubscription');
-            $router->get('/{subscription_id}/logs', 'Auth\SubscriptionController@GetSubscriptionLogs');
+            $router->delete('/{subscription_id}', 'VolistxTeam\VSkeletonKernel\Http\Controllers\SubscriptionController@DeleteSubscription');
+            $router->get('/', 'VolistxTeam\VSkeletonKernel\Http\Controllers\SubscriptionController@GetSubscriptions');
+            $router->get('/{subscription_id}', 'VolistxTeam\VSkeletonKernel\Http\Controllers\SubscriptionController@GetSubscription');
+            $router->get('/{subscription_id}/logs', 'VolistxTeam\VSkeletonKernel\Http\Controllers\SubscriptionController@GetSubscriptionLogs');
 
 
-            $router->delete('/{subscription_id}/personal-tokens/{token_id}', 'Auth\PersonalTokenController@DeletePersonalToken');
-            $router->put('/{subscription_id}/personal-tokens/{token_id}/reset', 'Auth\PersonalTokenController@ResetPersonalToken');
-            $router->get('/{subscription_id}/personal-tokens/{token_id}', 'Auth\PersonalTokenController@GetPersonalToken');
-            $router->get('/{subscription_id}/personal-tokens', 'Auth\PersonalTokenController@GetPersonalTokens');
+            $router->delete('/{subscription_id}/personal-tokens/{token_id}', 'VolistxTeam\VSkeletonKernel\Http\Controllers\PersonalTokenController@DeletePersonalToken');
+            $router->put('/{subscription_id}/personal-tokens/{token_id}/reset', 'VolistxTeam\VSkeletonKernel\Http\Controllers\PersonalTokenController@ResetPersonalToken');
+            $router->get('/{subscription_id}/personal-tokens/{token_id}', 'VolistxTeam\VSkeletonKernel\Http\Controllers\PersonalTokenController@GetPersonalToken');
+            $router->get('/{subscription_id}/personal-tokens', 'VolistxTeam\VSkeletonKernel\Http\Controllers\PersonalTokenController@GetPersonalTokens');
         });
 
         $router->group(['prefix' => 'plans'], function () use ($router) {
             $router->group(['middleware' => ['filter.json']], function () use ($router) {
-                $router->post('/', 'Auth\PlanController@CreatePlan');
-                $router->put('/{plan_id}', 'Auth\PlanController@UpdatePlan');
+                $router->post('/', 'VolistxTeam\VSkeletonKernel\Http\Controllers\PlanController@CreatePlan');
+                $router->put('/{plan_id}', 'VolistxTeam\VSkeletonKernel\Http\Controllers\PlanController@UpdatePlan');
             });
-            $router->delete('/{plan_id}', 'Auth\PlanController@DeletePlan');
-            $router->get('/', 'Auth\PlanController@GetPlans');
-            $router->get('/{plan_id}', 'Auth\PlanController@GetPlan');
+            $router->delete('/{plan_id}', 'VolistxTeam\VSkeletonKernel\Http\Controllers\PlanController@DeletePlan');
+            $router->get('/', 'VolistxTeam\VSkeletonKernel\Http\Controllers\PlanController@GetPlans');
+            $router->get('/{plan_id}', 'VolistxTeam\VSkeletonKernel\Http\Controllers\PlanController@GetPlan');
         });
 
         //TO DISCUSS THE LOGS HASSLE
         $router->group(['prefix' => 'logs'], function () use ($router) {
-            $router->get('/', 'Auth\AdminLogController@GetAdminLogs');
-            $router->get('/{log_id}', 'Auth\AdminLogController@GetAdminLog');
+            $router->get('/', 'VolistxTeam\VSkeletonKernel\Http\Controllers\AdminLogController@GetAdminLogs');
+            $router->get('/{log_id}', 'VolistxTeam\VSkeletonKernel\Http\Controllers\AdminLogController@GetAdminLog');
         });
     });
 });
