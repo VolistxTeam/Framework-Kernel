b10ec0016c4742bbd75550e1f109bd045e5aa2de
Scorias <mostafa.samy203@gmail.com>
2022-02-17 17:43:45 +0200

    add UserLogDTO.php and AdminLogDTO.php

---
+++ src/DataTransferObjects/AdminLogDTO.php
@@ -0,0 +1,31 @@
+<?php
+namespace Volistx\FrameworkKernel\DataTransferObjects;
+
+class AdminLogDTO extends DataTransferObjectBase
+{
+    public string $access_token_id;
+    public string $id;
+    public string $url;
+    public string $ip;
+    public string $method;
+    public ?string $user_agent;
+    public string $created_at;
+
+    public static function fromModel($userLog): self
+    {
+        return new self($userLog);
+    }
+    public function GetDTO(): array
+    {
+        return [
+            'id' => $this->id,
+            'access_token' => [
+                'id' => $this->access_token_id
+            ],
+            'url' => $this->url,
+            'ip' => $this->ip,
+            'method' => $this->method,
+            'user_agent' => $this->user_agent,
+        ];
+    }
+}
---
+++ src/DataTransferObjects/UserLogDTO.php
@@ -0,0 +1,31 @@
+<?php
+namespace Volistx\FrameworkKernel\DataTransferObjects;
+
+class UserLogDTO extends DataTransferObjectBase
+{
+    public string $subscription_id;
+    public string $id;
+    public string $url;
+    public string $ip;
+    public string $method;
+    public ?string $user_agent;
+    public string $created_at;
+
+    public static function fromModel($userLog): self
+    {
+        return new self($userLog);
+    }
+    public function GetDTO(): array
+    {
+        return [
+            'id' => $this->id,
+            'subscription' => [
+                'id' => $this->subscription_id
+            ],
+            'url' => $this->url,
+            'ip' => $this->ip,
+            'method' => $this->method,
+            'user_agent' => $this->user_agent,
+        ];
+    }
+}
