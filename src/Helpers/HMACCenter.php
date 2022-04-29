4619418a8150c76d6a287d065d4d8faefcac93a5
Scorias <mostafa.samy203@gmail.com>
2022-04-29 20:52:35 +0200

    fix hmac signature

--- src/Helpers/HMACCenter.php
+++ src/Helpers/HMACCenter.php
@@ -2,19 +2,30 @@
 
 namespace Volistx\FrameworkKernel\Helpers;
 
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\URL;
+
 class HMACCenter
 {
-    public static function sign($content, $key): string
+    public static function sign($content, $key): array
     {
-        $hashed_content = hash_hmac('sha256', json_encode($content), $key, true);
-        return base64_encode($hashed_content);
-    }
+        $method = Request::method();
+        $url = urlencode(URL::current());
+        $timestamp = strtotime("now");
+        $contentString = json_encode($content);
 
-    public static function getHeaders($content, $key): array
-    {
+        $valueToSign = $method
+            . $url
+            . $timestamp
+            . $contentString;
+
+        $signedValue = hash_hmac('sha256', $valueToSign, $key, true);
+
+        $signature = base64_encode($signedValue);
+
         return [
-            'X-HMAC-Timestamp' => strtotime("now"),
-            'X-HMAC-Content-Hash' => self::sign($content, $key)
+            'X-HMAC-Timestamp' => $timestamp,
+            'X-HMAC-Content-Hash' => $signedValue
         ];
     }
 }
