9713d5f138882aae38a13a476d39aac2379587ae
Scorias <mostafa.samy203@gmail.com>
2022-04-29 21:13:58 +0200

    fix bugs

--- src/Helpers/HMACCenter.php
+++ src/Helpers/HMACCenter.php
@@ -12,10 +12,12 @@ class HMACCenter
         $method = Request::method();
         $url = urlencode(URL::current());
         $timestamp = strtotime("now");
+        $nonce = substr(md5(uniqid(mt_rand(), true)), 0, 8);
         $contentString = json_encode($content);
 
         $valueToSign = $method
             . $url
+            . $nonce
             . $timestamp
             . $contentString;
 
@@ -25,7 +27,8 @@ class HMACCenter
 
         return [
             'X-HMAC-Timestamp' => $timestamp,
-            'X-HMAC-Content-Hash' => $signedValue
+            'X-HMAC-Content-Hash' => $signature,
+            'X-HMAC-Nonce' => $nonce
         ];
     }
 }
