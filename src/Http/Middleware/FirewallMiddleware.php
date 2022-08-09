b6142c4376a998ac0c8642b9ce223c8346404760
I'm Not Cry <44664655+Cryental@users.noreply.github.com>
2022-08-09 15:25:23 +0600

    Update FirewallMiddleware.php

--- src/Http/Middleware/FirewallMiddleware.php
+++ src/Http/Middleware/FirewallMiddleware.php
@@ -22,8 +22,6 @@ class FirewallMiddleware
         }
 
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With');
+            ->header('Access-Control-Allow-Origin', '*');
     }
 }
