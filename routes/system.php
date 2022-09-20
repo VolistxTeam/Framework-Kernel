b6053ab541100b2a6512c0103fcc33278c3370b3
Scorias <mostafa.samy203@gmail.com>
2022-09-20 02:54:53 +0200

    fix http verbs for correct docs

--- routes/system.php
+++ routes/system.php
@@ -17,10 +17,10 @@ $router->group(['prefix' => 'sys-bin'], function () use ($router) {
         $router->group(['prefix' => 'subscriptions'], function () use ($router) {
             $router->group(['middleware' => ['filter.json']], function () use ($router) {
                 $router->post('/', 'Volistx\FrameworkKernel\Http\Controllers\SubscriptionController@CreateSubscription');
-                $router->put('/{subscription_id}', 'Volistx\FrameworkKernel\Http\Controllers\SubscriptionController@UpdateSubscription');
+                $router->patch('/{subscription_id}', 'Volistx\FrameworkKernel\Http\Controllers\SubscriptionController@UpdateSubscription');
 
                 $router->post('/{subscription_id}/personal-tokens', 'Volistx\FrameworkKernel\Http\Controllers\PersonalTokenController@CreatePersonalToken');
-                $router->put('/{subscription_id}/personal-tokens/{token_id}', 'Volistx\FrameworkKernel\Http\Controllers\PersonalTokenController@UpdatePersonalToken');
+                $router->patch('/{subscription_id}/personal-tokens/{token_id}', 'Volistx\FrameworkKernel\Http\Controllers\PersonalTokenController@UpdatePersonalToken');
             });
 
             $router->delete('/{subscription_id}', 'Volistx\FrameworkKernel\Http\Controllers\SubscriptionController@DeleteSubscription');
@@ -30,7 +30,7 @@ $router->group(['prefix' => 'sys-bin'], function () use ($router) {
             $router->get('/{subscription_id}/usages', 'Volistx\FrameworkKernel\Http\Controllers\SubscriptionController@GetSubscriptionUsages');
 
             $router->delete('/{subscription_id}/personal-tokens/{token_id}', 'Volistx\FrameworkKernel\Http\Controllers\PersonalTokenController@DeletePersonalToken');
-            $router->put('/{subscription_id}/personal-tokens/{token_id}/reset', 'Volistx\FrameworkKernel\Http\Controllers\PersonalTokenController@ResetPersonalToken');
+            $router->patch('/{subscription_id}/personal-tokens/{token_id}/reset', 'Volistx\FrameworkKernel\Http\Controllers\PersonalTokenController@ResetPersonalToken');
             $router->get('/{subscription_id}/personal-tokens/{token_id}', 'Volistx\FrameworkKernel\Http\Controllers\PersonalTokenController@GetPersonalToken');
             $router->get('/{subscription_id}/personal-tokens', 'Volistx\FrameworkKernel\Http\Controllers\PersonalTokenController@GetPersonalTokens');
             $router->post('/{subscription_id}/personal-tokens/sync', 'Volistx\FrameworkKernel\Http\Controllers\PersonalTokenController@Sync');
@@ -39,7 +39,7 @@ $router->group(['prefix' => 'sys-bin'], function () use ($router) {
         $router->group(['prefix' => 'plans'], function () use ($router) {
             $router->group(['middleware' => ['filter.json']], function () use ($router) {
                 $router->post('/', 'Volistx\FrameworkKernel\Http\Controllers\PlanController@CreatePlan');
-                $router->put('/{plan_id}', 'Volistx\FrameworkKernel\Http\Controllers\PlanController@UpdatePlan');
+                $router->patch('/{plan_id}', 'Volistx\FrameworkKernel\Http\Controllers\PlanController@UpdatePlan');
             });
             $router->delete('/{plan_id}', 'Volistx\FrameworkKernel\Http\Controllers\PlanController@DeletePlan');
             $router->get('/', 'Volistx\FrameworkKernel\Http\Controllers\PlanController@GetPlans');
