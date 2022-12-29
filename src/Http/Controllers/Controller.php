9d0274eee3a53e427ece0bb4b895b7312912e75f
Cryental <contact@cryental.dev>
2022-12-30 02:47:03 +0600

    Fixed Bug

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -2,9 +2,9 @@
 
 namespace Volistx\FrameworkKernel\Http\Controllers;
 
-use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    public string $module;
+
 }
