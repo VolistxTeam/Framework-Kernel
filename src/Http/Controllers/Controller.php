e3781519cbd2527d22b56a24a102a0b859a27b8f
I'm Not Cry <44664655+Cryental@users.noreply.github.com>
2023-02-21 17:10:31 +0600

    Update Controller.php

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -2,9 +2,16 @@
 
 namespace Volistx\FrameworkKernel\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    use AuthorizesRequests;
+    use DispatchesJobs;
+    use ValidatesRequests;
+    
     public string $module;
 }
