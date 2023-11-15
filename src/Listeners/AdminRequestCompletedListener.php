dc860bb6da2bddb3a78340e874c78c3fa44dca51
Scorias <mostafa.samy203@gmail.com>
2023-11-15 03:57:25 +0200

    update listener

--- src/Listeners/AdminRequestCompletedListener.php
+++ src/Listeners/AdminRequestCompletedListener.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace App\Listeners;
-
 namespace Volistx\FrameworkKernel\Listeners;
 
 use Volistx\FrameworkKernel\Events\AdminRequestCompleted;
@@ -11,13 +9,25 @@ class AdminRequestCompletedListener
 {
     private IAdminLoggingService $adminLoggingService;
 
+    /**
+     * AdminRequestCompletedListener constructor.
+     *
+     * @param IAdminLoggingService $adminLoggingService
+     */
     public function __construct(IAdminLoggingService $adminLoggingService)
     {
         $this->adminLoggingService = $adminLoggingService;
     }
 
+    /**
+     * Handle the event.
+     *
+     * @param AdminRequestCompleted $event
+     * @return void
+     */
     public function handle(AdminRequestCompleted $event)
     {
+        // Create log using the provided service
         $this->adminLoggingService->CreateAdminLog($event->inputs);
     }
 }
--- src/Listeners/UserRequestCompletedListener.php
+++ src/Listeners/UserRequestCompletedListener.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Listeners;
-
 namespace Volistx\FrameworkKernel\Listeners;
 
 use Volistx\FrameworkKernel\Events\UserRequestCompleted;
@@ -11,13 +10,25 @@ class UserRequestCompletedListener
 {
     private IUserLoggingService $userLoggingService;
 
+    /**
+     * UserRequestCompletedListener constructor.
+     *
+     * @param IUserLoggingService $userLoggingService The user logging service.
+     */
     public function __construct(IUserLoggingService $userLoggingService)
     {
         $this->userLoggingService = $userLoggingService;
     }
 
+    /**
+     * Handle the UserRequestCompleted event.
+     *
+     * @param UserRequestCompleted $event The UserRequestCompleted event instance.
+     * @return void
+     */
     public function handle(UserRequestCompleted $event)
     {
+        // Create log using user service
         $this->userLoggingService->CreateUserLog($event->inputs);
     }
 }
